ffabe2bdbd77ef1438b75c00bffb5d09b76ba95a
dbdu <1797890817@qq.com>
2017-11-07 12:10:23 +0800

    add for ps-statement

--- DataBase6/src/com/ddbin/database/st/PsInsertDemo.java
+++ DataBase6/src/com/ddbin/database/st/PsInsertDemo.java
@@ -2,9 +2,11 @@ package com.ddbin.database.st;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class StDeleteDemo {
+public class PsInsertDemo {
 	public static final String DBDRIVER = "com.mysql.jdbc.Driver";
 	public static final String DBURL = "jdbc:mysql://localhost:3306/db_demo";
 	public static final String DBUSER = "root";
@@ -19,20 +21,23 @@ public class StDeleteDemo {
 		Connection conn = null;
 		conn = DriverManager.getConnection(DBURL, DBUSER, DBPWD);
 
-		// 创建statment
-		Statement st = conn.createStatement();
-		//String user="";
-		//String sql = "delete from stu where name = '"+user+"'";
-		String sql = "delete from stu where name = 'user4'";
-
+		// 创建Ps-statment
+		String sql = "insert into stu (name,age,cell,addr) values(?,?,?,?)";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		//设置四个占位符的值
+		ps.setString(1, "u002");
+		ps.setInt(2, 26);
+		ps.setString(3, "15600001235");
+		ps.setString(4, "南京");
+		
 		// 执行SQL语句
-		int i = st.executeUpdate(sql);
+		int i = ps.executeUpdate();
 		// 数据输出完毕
-		System.out.println("数据删除完毕!");
+		System.out.println("数据插入完毕!");
 		System.out.println(" i = " + i);
 
 		// 释放资源
-		st.close();
+		ps.close();
 		conn.close();
 
 	}
--- DataBase6/src/com/ddbin/database/st/PsUpdateDemo.java
+++ DataBase6/src/com/ddbin/database/st/PsUpdateDemo.java
@@ -2,9 +2,11 @@ package com.ddbin.database.st;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class StDeleteDemo {
+public class PsUpdateDemo {
 	public static final String DBDRIVER = "com.mysql.jdbc.Driver";
 	public static final String DBURL = "jdbc:mysql://localhost:3306/db_demo";
 	public static final String DBUSER = "root";
@@ -19,20 +21,19 @@ public class StDeleteDemo {
 		Connection conn = null;
 		conn = DriverManager.getConnection(DBURL, DBUSER, DBPWD);
 
-		// 创建statment
-		Statement st = conn.createStatement();
-		//String user="";
-		//String sql = "delete from stu where name = '"+user+"'";
-		String sql = "delete from stu where name = 'user4'";
-
+		// 创建ps-statment
+		String sql = "update stu set addr = ? where id = ?";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, "南京");
+		ps.setInt(2, 19);
 		// 执行SQL语句
-		int i = st.executeUpdate(sql);
+		int i = ps.executeUpdate();
 		// 数据输出完毕
-		System.out.println("数据删除完毕!");
+		System.out.println("数据更新完毕!");
 		System.out.println(" i = " + i);
 
 		// 释放资源
-		st.close();
+		ps.close();
 		conn.close();
 
 	}
